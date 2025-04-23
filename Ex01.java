778e7e40369df1cba92e2cd0d45f3b14f3c53160
Morganti00 <brunamorganti07@outlook.com>
2025-04-23 13:50:03 -0300

    Update Ex01.java

--- Ex01.java
+++ Ex01.java
@@ -10,3 +10,28 @@ public class Ex01 {
 	}
 
 }
+// i        i<=20       saída
+
+// 1        1<=20(v)    EU SOU UM JEDI EM JAVA
+// 2        2<=20(v)    EU SOU UM JEDI EM JAVA
+// 3        3<=20(v)    EU SOU UM JEDI EM JAVA
+// 4        4<=20(v)    EU SOU UM JEDI EM JAVA
+// 5        5<=20(v)    EU SOU UM JEDI EM JAVA
+// 6        6<=20(v)    EU SOU UM JEDI EM JAVA
+// 7        7<=20(v)   EU SOU UM JEDI EM JAVA
+// 8        8<=20(v)    EU SOU UM JEDI EM JAVA
+// 9        9<=20(v)   EU SOU UM JEDI EM JAVA
+// 10       10<=20(v)   EU SOU UM JEDI EM JAVA
+// 11       11<=20(v)   EU SOU UM JEDI EM JAVA
+// 12       12<=20(v)   EU SOU UM JEDI EM JAVA
+// 13       13<=20(v)   EU SOU UM JEDI EM JAVA
+// 14       14<=20(v)   EU SOU UM JEDI EM JAVA
+// 15       15<=20(v)   EU SOU UM JEDI EM JAVA
+// 16       16<=20(v)   EU SOU UM JEDI EM JAVA
+// 17       17<=20(v)   EU SOU UM JEDI EM JAVA
+// 18       18<=20(v)   EU SOU UM JEDI EM JAVA
+// 19       19<=20(v)   EU SOU UM JEDI EM JAVA
+// 20       20<=20(v)   EU SOU UM JEDI EM JAVA
+// 21       21<=20(f)>  
+........
+
